684fb1145d98559a888172f6963b0017880d0d9a
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-06-12 19:31:48 +0300

    test passed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+# input
+# for each input value
+#add number 
+# add current no to prevNo
 
 def print_fibonacci(length):
-    pass
+    sequence=[]
+    for i in range(length):
+       if i == 0:
+           sequence.append(0)
+       elif i==1 :
+           sequence.append(1)
+           
+       else :
+           sequence.append(sequence[-1]+sequence[-2])
+            
+    print(sequence)
+print_fibonacci(9)
